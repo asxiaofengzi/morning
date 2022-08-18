b89d27c782f928e1697001e2e59848500e6eba00
小丰子 <59054578+asxiaofengzi@users.noreply.github.com>
2022-08-18 17:17:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
+tset_id = os.environ["TEST_ID"]
+
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
