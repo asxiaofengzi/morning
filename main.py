b983055405d974a2a59f421eefaba44e8eea558b
小丰子 <59054578+asxiaofengzi@users.noreply.github.com>
2022-08-18 15:53:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
+  words = requests.get("https://api.tianapi.com/caihongpi/index?key=e7b26dd1be2bc86c14f3e2da5447bbdb")
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
